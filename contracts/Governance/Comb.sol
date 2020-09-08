e8c529cd5f2bb00bec3d3a70ff7547fec83a644a
combifinancedev <combifinancedev@gmail.com>
2020-09-08 13:40:53 +0700

    comp to comb

--- contracts/Governance/Comb.sol
+++ contracts/Governance/Comb.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.5.16;
 pragma experimental ABIEncoderV2;
 
-contract Comp {
+contract Comb {
     /// @notice EIP-20 token name for this token
-    string public constant name = "Compound";
+    string public constant name = "Combi";
 
     /// @notice EIP-20 token symbol for this token
-    string public constant symbol = "COMP";
+    string public constant symbol = "COMB";
 
     /// @notice EIP-20 token decimals for this token
     uint8 public constant decimals = 18;
